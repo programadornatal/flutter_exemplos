9342cfa992d967eb682fcb7fee210aa5a2f896b4
fabio <f4310$santos>
2018-08-20 21:18:41 -0300

    Drawer Inicial

---
+++ drawer_navigation/README.md
@@ -0,0 +1,10 @@
+# Flutter Drawer Navigation
+
+Nesse exemplo fiz um Drawer com menu e navegação entre páginas
+
+![image](images/foto1.png)![image](images/foto2.png)![image](images/foto3.png)
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ drawer_navigation/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Center(
+          child: new Text('Olá Mundo'),
+      )
+    );
+  }
+}
